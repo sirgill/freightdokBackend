6a8ec687ac529ca750eb4a388ec93ccf1b78b436
Vicky <vy4693@gmail.com>
2022-07-17 23:56:54 +0530

    [BND-007] : - updated model chrobinson load Schema

--- models/chRobinson.js
+++ models/chRobinson.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const CHRobinson = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+    },
     loadDetail: {
         type: Object,
         required: false
