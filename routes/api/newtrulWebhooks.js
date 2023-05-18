86453e1589200ca6380a5b7b6414b5f68dad6af6
vicky-zluri <vicky.y@zluri.com>
2023-05-19 00:25:19 +0530

    update ntwebhook status

--- routes/api/newtrulWebhooks.js
+++ routes/api/newtrulWebhooks.js
@@ -24,11 +24,11 @@ const newtrulWebhook = (req, res) => {
                 })
         }
         else if (event_type === 'OFFER_ACCEPTED' || event_type === 'ACCEPT_COUNTER_OFFER_SUCCESS' || event_type === 'ACCEPT_FINAL_OFFER_SUCCESS') {
-            Bids.updateOne({ loadNumber: id }, { status: true, offerStatus: 'OFFER_ACCEPTED', event_data })
+            Bids.updateOne({ loadNumber: id }, { status: true, offerStatus: event_type, event_data })
                 .then(resp => res.status(200).json({ success: true, message: 'Offer Status Updated at freightdok successfully !' }))
             return;
         } else if (event_type === 'OFFER_REJECTED' || event_type === 'REJECT_COUNTER_OFFER_SUCCESS' || event_type === 'REJECT_FINAL_OFFER_SUCCESS') {
-            Bids.updateOne({ loadNumber: id }, { status: false, offerStatus: 'OFFER_REJECTED', event_data })
+            Bids.updateOne({ loadNumber: id }, { status: false, offerStatus: event_type, event_data })
                 .then(resp => res.status(200).json({ success: true, message: 'Offer Status Updated at freightdok successfully !' }))
             return;
         }
