20c9db14bf36d84abf2db191a179b76706bd5571
jugalTiwari <times.jugal@gmail.com>
2023-12-11 21:58:17 +0530

    added pagination and total count in organizations enhanced query for the organizations get method.

--- routes/api/organizations.js
+++ routes/api/organizations.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const auth = require("../../middleware/auth");
 const router = express.Router();
@@ -5,30 +6,79 @@ const Organizations = require('../../models/Organizations');
 
 
 router.get('/', (req, resp) => {
-    const { orgId } = req.query
+    const { orgId = '', page = null, limit = null } = req.query
     let queryParams = {}
     if (orgId) {
-        Object.assign(queryParams, { _id: orgId })
+        Object.assign(queryParams, { _id: mongoose.Types.ObjectId(orgId) })
     }
-    Organizations.find(queryParams)
-        .populate('adminId')
+
+    const query = [
+        {
+            $match: queryParams
+        },
+        {
+            $lookup: {
+                from: 'users', // Replace 'users' with the actual collection name of Users
+                localField: 'adminId',
+                foreignField: '_id',
+                as: 'adminData'
+            }
+        },
+        {
+            $unwind: '$adminData'
+        },
+        {
+            $project: {
+                _id: 1,
+                name: 1,
+                adminId: 1,
+                otherOrgMetaData: 1,
+                adminData: {
+                    name: '$adminData.name',
+                    email: '$adminData.email',
+                    firstName: '$adminData.firstName',
+                    lastName: '$adminData.lastName',
+                }
+            }
+        }
+    ]
+
+    if (page && limit) {
+        query.push(
+            {
+                $skip: (+page - 1) * limit // Skip records based on page number and page size
+            },
+            {
+                $limit: +limit // Limit records per page
+            })
+    }
+
+    Organizations.aggregate(query)
         .exec((err, organizations) => {
             if (err) {
-                // Handle error
                 console.error(err);
-                return resp.status(400).send({ success: false, message: err.message });
+                resp.status(404).json({ data: [], message: 'Organizations not found' })
+            } else {
+                if (page && limit) {
+                    query.pop();
+                }
+                Organizations.aggregate([
+                    {
+                        $match: queryParams
+                    },
+                    {
+                        $group: {
+                            _id: null,
+                            totalCount: { $sum: 1 } // Calculate total count of matching documents
+                        }
+                    }
+                ]).exec((count_error, result) => {
+                    if (count_error) {
+                        return resp.json(count_error);
+                    }
+                    resp.status(200).json({ data: organizations, totalCount: result.length > 0 ? result[0].totalCount : 0 })
+                })
             }
-
-            // Iterate through each organization and add a new key with populated adminId data
-            const orgsWithAdminData = organizations.map(org => {
-                return {
-                    ...org.toObject(),
-                    adminData: org.adminId,// Adding a new key 'adminData' with populated user data
-                    adminId: org.adminId._id,
-                };
-            });
-            // Now orgsWithAdminData contains each organization document with an added 'adminData' key
-            return resp.status(200).send({ success: true, data: orgsWithAdminData });
         });
 })
 
