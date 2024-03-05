61b5df4df89f68edc5e40cdde327754348ab84a2
jugalTiwari <times.jugal@gmail.com>
2024-03-05 18:16:12 +0530

    Added save, update and getById endpoints in defaultRolePermission

--- routes/api/rolePermission.js
+++ routes/api/rolePermission.js
@@ -1,66 +1,72 @@
 const express = require("express");
 const auth = require("../../middleware/auth");
 const router = express.Router();
-const { Roles, Permissions, Dashboards } = require('../../models/RolePermission');
+const DefaultRolePermission = require('../../models/DefaultRolePermissions');
 const mongoose = require("mongoose");
+const DefaultRolePermissions = require("../../models/DefaultRolePermissions");
 
 router.get('/rolesAndPermissions', auth, async (req, res) => {
     try {
-        const roles = await Roles.find({}).sort({ name: 1 });
-        const dashboards = await Dashboards.find({}).sort({ name: 1 });
-        res.status(200).json({ roles, dashboards })
+        const rolePermissions = await DefaultRolePermission.find({})
+        res.status(200).json({ permissions: rolePermissions, success: true })
     } catch (error) {
-        res.status(400).json({ data: {}, message: error.message })
+        res.status(400).json({ data: [], message: error.message })
     }
 })
 
-router.get('/all', auth, (req, res) => {
+router.get('/getRoleAndPermissionById', auth, async (req, res) => {
     try {
-        Permissions.find()
-            .populate(['dashboard', 'role'])
-            .exec((err, result) => {
-                if (err) {
+        const rolePermission = await DefaultRolePermission.findById({ _id: req.query._id })
+        const allRoles = await DefaultRolePermission.find()
+        res.status(200).json({ data: rolePermission, allRoles })
+    } catch (error) {
+        console.log(error.message)
+        res.status(400).json({ message: error.message })
+    }
+})
 
-                } else {
-                    res.status(200).json({ data: result })
+router.post('/', auth, (req, res) => {
+    const { roleName, permissions } = req.body;
+    try {
+        const defaultRolePermission = new DefaultRolePermissions({ roleName, permissions });
+        defaultRolePermission.save()
+            .then(result => {
+                res.status(201).json({ message: 'Role Created', data: result });
+            })
+            .catch(err => {
+                if (err.message.includes('roleName_1 dup')) {
+                    return res.status(403).json({ message: 'Role Name is already present in freightdok', _dbError: err.message });
                 }
+                res.status(400).json({ message: 'Something went wrong', _dbError: err.message });
             })
     } catch (error) {
-
+        res.status(400).json({ message: 'Role not saved', _dbError: err.message });
     }
 })
 
 //Create a new Role Dashboard Mapping
-router.post('/', auth, async (req, res) => {
-    const { dashboardId, roleId, permissions } = req.body;
-    if (!dashboardId || !roleId) {
+router.put('/', auth, async (req, res) => {
+    const { roleId: _id, permissions, dashboard, roleName } = req.body;
+    if (!_id) {
         return res.status(400).json({ message: 'Missing required fields in request body' });
     }
 
-    const isValidDashboardId = mongoose.Types.ObjectId.isValid(dashboardId);
-    const isValidRoleId = mongoose.Types.ObjectId.isValid(roleId);
+    const isValidRoleId = mongoose.Types.ObjectId.isValid(_id);
+    const data = await DefaultRolePermission.findOne({ _id });
 
-    if (!isValidDashboardId || !isValidRoleId) {
-        return res.status(400).json({ message: 'Invalid dashboard or role ID' });
+    if (!isValidRoleId || !data.permissions) {
+        return res.status(400).json({ message: 'Invalid role ID or Role not available' });
     }
 
+    const newPermissions = { ...(data.permissions || {}), [dashboard]: permissions };
 
     try {
-        const dashboard = await Dashboards.findById(dashboardId);
-        const role = await Roles.findById(roleId);
-
-        if (!dashboard || !role) {
-            return res.status(404).json({ message: 'Dashboard or role not found' });
-        }
-
-        const dashboardRole = new Permissions({
-            dashboard: dashboardId,
-            role: roleId,
-            permissions
-        });
-
-        await dashboardRole.save();
-        res.status(201).json({ message: 'Dashboard role mapping created successfully' });
+        DefaultRolePermission.updateOne({ _id }, { permissions: newPermissions, roleName }, (err) => {
+            if (err) {
+                return res.status(400).json({ message: 'Something went wrong.', _dbError: err.message })
+            }
+            res.status(201).json({ message: 'Dashboard role mapping Updated successfully' });
+        })
 
     } catch (error) {
         console.log(error.message)
